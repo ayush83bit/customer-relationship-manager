b8a52082209d1f3f6ae1b92fa5f331d1978f3894
Ayush Dubey <ayushdubey1102@gmail.com>
2025-09-20 18:20:34 +0530

    Update LeadDto.java

--- src/main/java/com/ayush/crm/BizNest/LeadDto.java
+++ src/main/java/com/ayush/crm/BizNest/LeadDto.java
@@ -8,7 +8,7 @@ public class LeadDto {
     private String status;
     private String company;
 
-    // Getters and Setters
+    
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
